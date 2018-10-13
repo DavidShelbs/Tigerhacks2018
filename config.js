bf73c42ac5d4460f46ace2c476aca9fd4ee6b433
DNSNWB <dnsnwb@mst.edu>
2018-10-13 15:06:35 -0500

    tiger hacks

---
+++ config.js
@@ -0,0 +1,7 @@
+var config = {
+  apiKey: "AIzaSyBgcSHxbo93P-N9Sh-ZIqgzvwicHDk_Skc",
+  authDomain: "fir-ui-demo-84a6c.firebaseapp.com",
+  databaseURL: "https://fir-ui-demo-84a6c.firebaseio.com",
+  storageBucket: "fir-ui-demo-84a6c.appspot.com",
+};
+firebase.initializeApp(config);
